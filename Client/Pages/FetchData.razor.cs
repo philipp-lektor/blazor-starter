4439e0f5f356a89f2dc126ebac355c184f850350
Philipp Losbichler <philipp.losbichler@agravity.io>
2024-06-07 09:55:54 +0200

    Move the code to extra file

---
+++ Client/Pages/FetchData.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Client.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts = new WeatherForecast[] { };
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("/api/WeatherForecast") ?? new WeatherForecast[] { };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
